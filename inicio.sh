4911b7adf2e51803af9048767031138d2baf0066
Daniel Barbieri <90072728+DHB1985@users.noreply.github.com>
2022-09-07 09:04:44 -0300

    add/script

---
+++ inicio.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd deploy
+
+docker stop helloNode helloGolang helloNginx 
+
+docker rm helloNginx helloNode helloGolang
+
+docker rmi deploy_hello-node:latest deploy_hello-golang:latest deploy_hello-nginx:latest
+
+docker compose up -d
+
+docker ps
